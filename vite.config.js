f79e773123c2a7915a9036f7b79f4ad6d3aa207a
Gene <64043816+Gene-avatech@users.noreply.github.com>
2023-07-21 09:15:15 -0700

    Open network IP

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,31 @@
-import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig, loadEnv } from 'vite';
+import path from 'path';
 import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  root: './src/client',
-});
+
+// main config export
+export default ({ mode }) => {
+
+	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+	const plugins = [vue()]
+
+	return defineConfig({
+		server: {
+			host: '0.0.0.0' /* allow testing on local IP for iPad/phone/net */,
+		},
+		plugins: plugins,
+		resolve: {
+			alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+			},
+		},
+		root: './src/client',
+		build: {
+			outDir: path.join(__dirname, 'dist/production'),
+		},
+		// eslint-disable-next-line no-undef
+		base: process.env.VITE_PATH,
+	});
+}
